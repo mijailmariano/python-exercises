c8eec7a785845d3f7b99b7b00caab8fd93dd6953
Mijail Q. Mariano <mijail.mariano@gmail.com>
2022-05-26 17:57:40 -0400

    updated

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -6,8 +6,6 @@
 # You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). 
 # If price for a movie per day is 3 dollars, how much will you have to pay?
 
-from pickle import NONE
-
 
 little_mermaid = 3
 brother_bear = 5
